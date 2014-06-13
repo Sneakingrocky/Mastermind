3144dbcdf21c627f12763c4e23c408c138aaf11a
Erica <garcialuck08@gmail.com>
2014-06-13 09:28:30 -0500

    Working Mastermind
    "Mastermind is a two-player code-breaking game where one player makes a
    code (in your program this will be the human player) and another player
    guesses the code (in your program this will be the computer player).
    Your task is to create a game that allows a human player to play
    against the computer. The computer should be able to guess the correct
    code (created by the human player) within 10 turns. The code is made up
    of four colors and can include duplicates. There are six available
    colors from which to choose: red, green, orange, yellow, blue, purple."

--- mastermind.rb
+++ mastermind.rb
@@ -1,33 +1,32 @@
-#TO DO
-#Need to program computer to have ability to guess after the first round guess "1122"
-#Eliminate possibilies that are not possible
-#Assign colors to numbers
 
-
+#human gives computer answer>computer calculates the score between its guess and the answer
 
 #Separates matches and near matches for computer
 
-def score(num1, num2)
+
+def score(color1, color2)
   matches = 0
   near_matches = 0
+  color1 = color1.dup #copying arrays to save from .delete(" ") method
+  color2 = color2.dup
 
   for i in 0..3
-    if num1[i] == num2[i]
+    if color1[i] == color2[i]
       matches = matches + 1
-      num1[i] = " "
-      num2[i] = " "
+      color1[i] = " "
+      color2[i] = " "
 
     end
   end
 
-  num1.delete(" ")
-  num2.delete(" ")
+  color1.delete(" ")
+  color2.delete(" ")
 
-  num1.each do |peg1|
-    num2.each_with_index do |peg2, i|
+  color1.each do |peg1|
+    color2.each_with_index do |peg2, i|
       if peg1 == peg2
         near_matches = near_matches + 1
-        num2.delete_at(i)
+        color2.delete_at(i)
 
         break
 
@@ -40,20 +39,37 @@ end
 #Sets number of rounds the computer has to guess the correct # to 10
 #Computer starts at "1122" for an optimal guess
 
+
+puts "Can I have your secret color code?"
+secret_color_code = gets.chomp.chars # chomp deletes new line and chars returns as an array
+
+
+#permutation of possible guesses
+colors = ["r", "o", "y", "g", "b", "p"]
+possibilities = colors.repeated_permutation(4).to_a
+
+
+#Computers guess
+guess = ["r", "r", "o", "o"]
+puts "My guess is rroo!"
+
 10.times do |round|
-  puts "This is round #{1 + round}"
-  puts "My guess is 1122!"
+  puts "This is round #{round}"
+  matches, near_matches = score(secret_color_code, guess)
 
-  puts "How many are correct in the correct spot?"
-  correct = gets.to_i
-
-  puts "How many are correct in the wrong spot?"
-  wrong_spot = gets.to_i
-
-  if correct == 4
-    puts "I did it! I'm so smart."
-    break
+  if matches == 4
+    puts "I did it! I am so smart."
+    break #to stop loop
   elsif round == 9
-    puts "I lose. At least I have my good looks."
+    puts "I lose. At least I still have my good looks."
+    break #to stop loop
   end
+
+  possibilities.delete_if do |possibility|
+    new_matches, new_near_matches = score(possibility, guess)
+    [matches, near_matches] != [new_matches, new_near_matches]
+  end
+
+  guess = possibilities[0] #this is guessing the first possibility
+  puts "My guess is #{possibilities[0]}"
 end
